45b671cf23b7d78379cb15f48e803df59de80fc2
Muthukaruppan <128952571+Muthukaruppan2005@users.noreply.github.com>
2025-06-13 11:09:08 +0530

    Create Insertion at End (creation,display).java

---
+++ LinkedList/Single LinkedList/Insertion at End (creation,display).java
@@ -0,0 +1,50 @@
+import java.util.*;
+public class main{
+  public static void main(String[]args){
+    Scanner n=new Scanner(System.in);
+    linkedlist l=new linkedlist();
+    int a=n.nextInt();
+    if(a==0)
+      return;
+    for(int i=0;i<a;i++){
+      int num=n.nextInt();
+      l.create(num);
+    }
+    l.display();
+  }
+}
+
+class linkedlist{
+  
+  Node head=null,curr=null;
+  void create(int n){
+    Node newnode=new Node(n);
+    if(head==null){
+      head=newnode;
+      curr=newnode;
+    }else{
+      curr.next=newnode;
+      curr=curr.next;
+    }
+      
+  }
+  
+  void display(){
+    Node temp=head;
+    while(temp!=null){
+      System.out.print(temp.data+" ");
+      temp=temp.next;
+    }
+  }
+}
+  
+  
+class Node{
+  int data;
+  Node next;
+  Node(int val){
+    this.data=val;
+    this.next=null;
+  }
+  
+}
