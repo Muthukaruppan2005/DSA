369c8f6a8f92f6f9633a6c24cacd63435973a84e
Muthukaruppan <128952571+Muthukaruppan2005@users.noreply.github.com>
2025-06-13 12:12:52 +0530

    Create Delete at Beginning.java

---
+++ LinkedList/Single LinkedList/Delete at Beginning.java
@@ -0,0 +1,78 @@
+import java.util.*;
+public class main{
+  public static void main(String[]args){
+    Scanner n=new Scanner(System.in);
+    linkedlist l=new linkedlist();
+    int a=n.nextInt();
+    if(a==0){
+      System.out.print("List is empty");
+      return;
+    }
+    for(int i=0;i<a;i++){
+      int num=n.nextInt();
+      l.create(num);
+    }
+    l.delete();
+    l.display();
+  }
+}
+
+class linkedlist{
+  
+  Node head=null,curr=null;
+  void create(int n){
+    Node newnode=new Node(n);
+    if(head==null){
+      head=newnode;
+      curr=newnode;
+    }else{
+      curr.next=newnode;
+      curr=curr.next;
+    }
+      
+  }
+  //check if next of head is null 
+  //temp=head
+  //head of next = head
+  //temp of next=null
+  void delete(){
+    if(head.next==null){
+      System.out.print("List is empty");
+      head=null;
+      return;
+    }else{
+      Node temp=head;
+      head=head.next;
+      temp.next=null;
+    }
+  }
+  
+  void display(){
+    Node temp=head;
+    while(temp!=null){
+      System.out.print(temp.data+" ");
+      temp=temp.next;
+    }
+  }
+}
+  
+  
+class Node{
+  int data;
+  Node next;
+  Node(int val){
+    this.data=val;
+    this.next=null;
+  }
+  
+}
+
+-----------------------------
+  /*
+  Sample Input:
+4
+10 20 30 40
+
+Sample Output:
+20 30 40
+  */
